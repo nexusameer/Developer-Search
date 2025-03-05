a99ceadb717d5c340a4907894645652f40f0aeff
Muhammad Ameer Hamza <129995036+nexusameer@users.noreply.github.com>
2025-03-05 16:25:58 +0500

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -169,7 +169,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'nexusameer@gmail.com'
-EMAIL_HOST_PASSWORD = 'cqpopczyubkescma'
+EMAIL_HOST_PASSWORD = 'pmgpkzdpvjmwudoi'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
