d3ebcc2ddb65b3dc7493ef2feab6f526bb814689
nexusameer <ameerk10fw@gmail.com>
2025-03-05 18:47:51 +0500

    prod issue resolved

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a)r4f)ujn&__#9df=j4^20jnd0rg$&!tiub_!b@(jgnr%g5w+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
